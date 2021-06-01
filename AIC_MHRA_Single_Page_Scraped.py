ce947ce9ba22807af82aac78628acbae3cd6679f
Preeti Singh <84151937+preetisingh124@users.noreply.github.com>
2021-06-01 23:18:29 +0530

    Delete AIC_MHRA_Single_Page_Scraped.py

--- AIC_MHRA_Single_Page_Scraped.py
+++
@@ -1,6 +0,0 @@
-from Scraping_functions  import Extractor, Pages, Scraper
-s = Scraper()
-p = s.get_parsed_data("https://aic.mhra.gov.uk/era/pdr.nsf/name?openpage&start=1&count=200")
-h,r = Extractor.table_extractor(p)
-s.make_dataframe(header = h, extracted_rows=r)
-s.save('aic_MHRA.tsv')
