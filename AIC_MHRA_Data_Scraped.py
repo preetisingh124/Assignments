f2f09e253afc78c788c6f32cc5b49d6d5b58d359
Preeti Singh <84151937+preetisingh124@users.noreply.github.com>
2021-06-01 12:16:08 +0530

    Delete AIC_MHRA_Data_Scraped.py

--- AIC_MHRA_Data_Scraped.py
+++
@@ -1,6 +0,0 @@
-from Scraping_basics_functions import data_parser,fetch_table_by_class, save_data_frame,find_links_of_all_pages,Scraped_all_pages
-
-parsed_data = data_parser("https://aic.mhra.gov.uk/era/pdr.nsf/name?openpage&start=1&count=200")
-all_pages_link = find_links_of_all_pages(parsed_data, 'textbox')
-head, all_pages= Scraped_all_pages(all_pages_link)
-save_data_frame(all_pages,head,'all_pages_mhrd2.tsv')
